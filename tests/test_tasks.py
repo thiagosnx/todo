4350d40233f57d4795cf2ab057340c455394b461
thiagosnx <thiagosantanacq@gmail.com>
2025-01-01 13:19:04 -0300

    feat: adicionando testes de put e delete

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -25,3 +25,32 @@ def test_get_tasks():
     if tasks:
         assert "titulo" in tasks[0]
         assert "estado" in tasks[0]
+
+# def test_get_task_by_id():
+#     response = client.post("/tasks/", json=task_data)
+#     assert response.status_code == 201
+#     task_id = response.json()["id"]
+
+#     response = client.post(f"tasks/{task_id}", json=task_data)
+
+def test_update_task():
+    response = client.post("/tasks/", json=task_data)
+    assert response.status_code == 201
+    task_id = response.json()["id"]
+
+    updated_data = {"estado": "concluida"}
+    response = client.put(f"tasks/{task_id}", json=updated_data)
+    assert response.status_code == 200
+    updated_task = response.json()
+    assert updated_task["estado"] == "concluida"
+
+def test_delete_task():
+    response = client.post("/tasks/", json=task_data)
+    assert response.status_code == 201
+    task_id = response.json()["id"]
+
+    response = client.delete(f"/tasks/{task_id}")
+    assert response.status_code == 204 
+
+    response = client.get(f"/tasks/{task_id}")
+    assert response.status_code == 404
